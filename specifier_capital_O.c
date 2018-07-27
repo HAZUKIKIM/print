329d5a67e13f7b2abf5d6b818ed15ba84e2da03b
Hazuki Miyake <hmiyake@e1z3r2p22.42.us.org>
2018-07-26 17:43:52 -0700

    test

--- specifier_capital_O.c
+++ specifier_capital_O.c
@@ -6,7 +6,7 @@
 /*   By: hmiyake <hmiyake@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/25 11:52:38 by hmiyake           #+#    #+#             */
-/*   Updated: 2018/07/26 17:10:00 by hmiyake          ###   ########.fr       */
+/*   Updated: 2018/07/26 17:41:14 by hmiyake          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int specifier_capital_o(va_list ap, char *save_f, int *w_n_p, char *save_l)
     uintmax_t num;
     int i;
 
-    num = (long)va_arg(ap, uintmax_t);
+    num = (unsigned long)va_arg(ap, uintmax_t);
     if (count5(num, w_n_p) >= w_n_p[0] && count5(num, w_n_p) >= w_n_p[1])
         i = count5(num, w_n_p) + o_c_is_biggest(save_f, w_n_p, num);
     else if (w_n_p[1] > count5(num, w_n_p) && w_n_p[1] >= w_n_p[0]) 
